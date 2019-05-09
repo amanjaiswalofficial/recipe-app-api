327c4dfd73b0bb8e0b93396b2b56dc0e2a2264df
Aman <amanjai01.gmail.com>
2019-05-09 15:42:15 +0530

    Started working on user

--- app/app/settings.py
+++ app/app/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'core',
+    'user',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
